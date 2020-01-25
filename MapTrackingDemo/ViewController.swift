1b8879138bd6f2e2dee99997285b31d063e64baf
Paul Wilkinson <paulw@wilko.me>
2020-01-26 08:21:56 +1100

    Initial Commit

---
+++ MapTrackingDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapTrackingDemo
+//
+//  Created by Paul Wilkinson on 26/1/20.
+//  Copyright © 2020 Paul Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
