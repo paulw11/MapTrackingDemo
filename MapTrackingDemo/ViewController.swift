0bcf334d7b571473a0eb21a93c993b7e8dd5e47e
Paul Wilkinson <paulw@wilko.me>
2020-01-26 08:26:21 +1100

    Add mapping

--- MapTrackingDemo/ViewController.swift
+++ MapTrackingDemo/ViewController.swift
@@ -6,13 +6,22 @@
 //  Copyright © 2020 Paul Wilkinson. All rights reserved.
 //
 
+import CoreLocation
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var map: MKMapView!
+    
+    private var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.locationManager.requestWhenInUseAuthorization()
+        self.map.showsUserLocation = true
+        self.map.userTrackingMode = .follow
     }
 
 
